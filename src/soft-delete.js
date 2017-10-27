c0585e59b4aaab81fa02e6c54ac4347063997d7d
Josh Furie <jfurie@gmail.com>
2017-10-27 16:30:59 -0700

    fix

--- src/soft-delete.js
+++ src/soft-delete.js
@@ -53,8 +53,7 @@ export default (Model, { deletedAt = 'deletedAt', _isDeleted = '_isDeleted', scr
   // Emulate default scope but with more flexibility.
   const queryNonDeleted = {
     or: [
-      { _isDeleted: { exists: false } },
-      { _isDeleted: false },
+      { _isDeleted: { neq: true } },
     ],
   };
 
